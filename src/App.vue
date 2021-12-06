303081ce337964c76585534b8b4e24f4d227a2ab
mammad riyahi <mmriyahi7337@yahoo.com>
2021-12-06 17:33:21 +0330

    make a login form work right

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <navbar />
-  {{$store.state}}
   <router-view class="container"/>
 </template>
 
@@ -29,7 +28,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="login">
-    <h1>Login</h1>
-    <button class="btn btn-success" @click="dologin">Login</button>
+    <div class="row">
+      <div class="col-lg-4 mx-auto border shadow rounded p-4 mt-3">
+          <h1 class="text-center mt-3 mb-4">Login</h1>
+           <form @submit.prevent="dologin">
+          <div class="form-group">
+            <label for="usernameInput">Username</label>
+            <input type="text" class="form-control" id="usernameInput" v-model="username">
+          </div>
+          <div class="form-group">
+            <label for="passwordInput">Password</label>
+            <input type="password" class="form-control" id="passwordInput" v-model="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Login</button>
+          </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
+    data() {
+      return {
+        username:'',
+        password:'',
+      }
+    },
   
-    methods:{
-        dologin(){
-            this.$store.commit("login","abc123")
-            this.$router.push("/profile")
+    methods: {
+        dologin() {
+            console.log(this.username)
+            console.log(this.password)
+            // this.$store.commit("login","abc123")
+            // this.$router.push("/profile")
 
         }
     }
